07489046d0d0725c7fd8929cbb12282210c20296
fathyafathazz <fathyafathazzra@gmail.com>
2025-01-05 22:47:12 +0700

    add include credential

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify(data),
     })
       .then((response) => {
